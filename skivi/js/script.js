8016af569ccedb6678dd7242600344fd550f4f76
DamianTeodora <75755978+DamianTeodora@users.noreply.github.com>
2021-04-11 13:40:44 +0300

    Delete skivi directory

--- skivi/js/script.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("sideNavigation").style.width = "20px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
- 
-function closeNav() {
-    document.getElementById("sideNavigation").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-}
